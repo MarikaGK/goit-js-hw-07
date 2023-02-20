9c3593c052a907776ce94c150d3e94cd701991f1
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-02-20 22:34:02 +0100

    add gallery01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector("div.gallery");
+
+const firstGallery = galleryItems
+  .map(
+    (image) => `<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+galleryDiv.insertAdjacentHTML("afterbegin", firstGallery);
+
+galleryDiv.addEventListener("click", openModal);
+
+function openModal(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery__image")) {
+    
+    const imageUrl = e.target.dataset.source;
+    const instance = basicLightbox.create(
+      `
+      <img src="${imageUrl}" width="800" height="600">`,
+      {
+        onShow: () => {
+          window.addEventListener("keydown", onEscKeydown);
+        },
+        onClose: () => {
+          window.removeEventListener("keydown", onEscKeydown);
+        },
+      }
+      );
+      
+      const onEscKeydown = (event) => {
+        if (event.code === "Escape") {
+          instance.close();
+        }
+      };
+
+    instance.show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector("ul.gallery");
+
+const secondGallery = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+  )
+  .join("");
+
+galleryDiv.insertAdjacentHTML("afterbegin", secondGallery);
