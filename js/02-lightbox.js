cf11c67cba01f9ad3806e91c3dda103809803517
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-02-24 16:18:39 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,26 +14,11 @@ const secondGallery = galleryItems
 
 galleryDiv.insertAdjacentHTML("afterbegin", secondGallery);
 
-galleryDiv.addEventListener("click", openModal);
+var lightbox = new SimpleLightbox(".gallery a", {
+  nav: false,
+  captions: true,
+  captionsData: "alt",
+  captiondelay: 250,
+  showCounter: false,
+});
 
-function openModal(e) {
-  e.preventDefault();
-
-  if (e.target.classList.contains("gallery__image")) {
-    var lightbox = new SimpleLightbox(".gallery a", {
-      nav: false,
-      captions: true,
-      captionsData: "alt",
-      captiondelay: 250,
-      showCounter: false,
-    });
-
-    const lightboxWrapper = document.querySelector(".sl-wrapper");
-    const lightboxOverlay = document.querySelector(".sl-overlay");
-
-    lightbox.on("close.simplelightbox", function () {
-      lightboxWrapper.remove();
-      lightboxOverlay.remove();
-    });
-  }
-}
